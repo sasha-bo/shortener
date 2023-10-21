480d8a5caa1156d1a0655665656ad3367e8c9e8b
Alexandr Bondarev <xbndrv@gmail.com>
2023-10-21 19:39:40 +0300

    cs-fixer

--- src/Parts/HtmlTag.php
+++ src/Parts/HtmlTag.php
@@ -12,8 +12,9 @@ class HtmlTag extends AbstractPart
     public function getName(): string
     {
         $ret = '';
-        preg_replace_callback('%^</?([^>\s]+)%', function(array $matches) use (&$ret): void {
+        preg_replace_callback('%^</?([^>\s]+)%', function (array $matches) use (&$ret): string {
             $ret = $matches[1];
+            return $matches[1];
         }, $this->value);
 
         return strtolower($ret);
